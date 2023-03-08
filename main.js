b3e8c2ef771bb837cb8ef2e8a4802581e83cf0b7
lubigsis <lucre.baluc@gmail.com>
2023-03-08 16:11:20 -0300

    Edito CSS y empiezo a trabajar con las funciones de drag & drop

--- main.js
+++ main.js
@@ -4,9 +4,46 @@ let columns = 3;
 let fichaActual;
 let otraFicha; //ficha en blanco.
 let turnos = 0;
-//orden de cada img. Ver si las coloqué bien
-//let imgOrden =  ["3", "2", "1", "6", "5", "4", "9", "8", "7"];
-let imgOrden =  ["9", "8", "7", "6", "5", "4", "3", "2", "1"];
+//Comento el array que tiene las img ordenadas; incluye la ficha blanca en el index 8 (img 1)
+//let imgOrden =  ["9", "8", "7", "6", "5", "4", "3", "2", "1"];
+
+//Array desordenado:
+let imgOrden = ["4", "2", "8", "5", "3", "6", "7", "9", "1"];
+
+
+//--------------------------------------------------------------------FUNCIONES---------------------
+//la img q' va a ser arrastrada
+function dragStart() {
+    fichaActual = this; 
+}
+
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+
+function dragEnter(e) {
+    e.preventDefault();
+}
+
+
+function dragLeave() {
+
+}
+
+//cuando dejo la img
+function dragDrop() {
+    otraFicha = this; 
+}
+
+
+function dragEnd() {
+    if (!otraFicha.src.includes("1.jpg")) {
+        return;
+    }                                      
+    
+    }
 
 //-----------------------------------------------------------------------------------
 
@@ -19,14 +56,18 @@ window.onload = function() {
             ficha.id = r.toString() + "-" + c.toString();
             ficha.src = imgOrden.shift() + ".jpg";
 
+//-------------------------------------------DRAG-----------------------------------
+
+            ficha.addEventListener("dragstart", dragStart);  
+            ficha.addEventListener("dragover", dragOver);    
+            ficha.addEventListener("dragenter", dragEnter);  
+            ficha.addEventListener("dragleave", dragLeave);  
+            ficha.addEventListener("drop", dragDrop);        
+            ficha.addEventListener("dragend", dragEnd);      
+
             document.getElementById("board").append(ficha);
 
-//--------------------------------------------DRAG 
-           
-
-            
-
-        }
+         }
     }
 };
 
